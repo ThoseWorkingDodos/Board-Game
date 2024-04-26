d2ea30b59e73c299a41360b6734abc35a7de15c4
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-04-26 16:29:52 -0400

    Added new pickup script and rigidbodies to all objects

---
+++ Assets/Scripts/ObjectPickup.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+    public float forceAmount = 500f;
+
+    [SerializeField]Rigidbody   selectedRigidbody;
+    Camera      targetCamera;
+    Vector3     originalScreenTargetPosition;
+    Vector3     originalRigidbodyPos;
+    float       selectionDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (!targetCamera)
+            return;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //Check if we are hovering over Rigidbody, if so, select it
+            selectedRigidbody = GetRigidbodyFromMouseClick();
+        }
+        if (Input.GetMouseButtonUp(0) && selectedRigidbody)
+        {
+            //Release selected Rigidbody if there any
+            selectedRigidbody = null;
+        }
+
+        if (Input.GetKey(KeyCode.E) && selectedRigidbody )
+        {
+            selectedRigidbody.transform.Rotate(new Vector3(0,5,0));
+        }
+        if (Input.GetKey(KeyCode.Q) && selectedRigidbody)
+        {
+            selectedRigidbody.transform.Rotate(new Vector3(0, -5, 0));
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (selectedRigidbody)
+        {
+            Vector3 mousePositionOffset = targetCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance)) - originalScreenTargetPosition;
+            selectedRigidbody.velocity = (originalRigidbodyPos + mousePositionOffset - selectedRigidbody.transform.position) * forceAmount * Time.deltaTime;
+        }
+    }
+
+    Rigidbody GetRigidbodyFromMouseClick()
+    {
+        Ray ray = targetCamera.ScreenPointToRay(Input.mousePosition);
+        bool hit = Physics.Raycast(ray, out RaycastHit hitInfo);
+        if (hit)
+        {
+            if (hitInfo.collider.gameObject.GetComponent<Rigidbody>())
+            {
+                selectionDistance = Vector3.Distance(ray.origin, hitInfo.point);
+                originalScreenTargetPosition = targetCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance));
+                originalRigidbodyPos = hitInfo.collider.transform.position;
+                return hitInfo.collider.gameObject.GetComponent<Rigidbody>();
+            }
+        }
+
+        return null;
+    }
+}
