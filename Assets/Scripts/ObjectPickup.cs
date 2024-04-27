cfd260f8eedae4c2887ee4e024f8556c77a17848
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-04-26 21:08:27 -0400

    Added Dice roll and fixed rigidbody physics

--- Assets/Scripts/ObjectPickup.cs
+++ Assets/Scripts/ObjectPickup.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 
 public class ObjectPickup : MonoBehaviour
 {
-    public float forceAmount = 500f;
+    public float forceAmount = 10f;
 
     [SerializeField]Rigidbody   selectedRigidbody;
     Camera      targetCamera;
     Vector3     originalScreenTargetPosition;
     Vector3     originalRigidbodyPos;
     float       selectionDistance;
-
+    public bool cardPicked;
+    public float rayDist;
+    [SerializeField] private LayerMask layer;
     // Start is called before the first frame update
     void Start()
     {
         targetCamera = GetComponent<Camera>();
+        rayDist = 50f;
     }
 
     void Update()
@@ -36,10 +39,23 @@ public class ObjectPickup : MonoBehaviour
         {
             selectedRigidbody.transform.Rotate(new Vector3(0,5,0));
         }
+
+        if (Input.GetKey(KeyCode.Z) && selectedRigidbody)
+        {
+            selectedRigidbody.transform.position = new Vector3 (0,12,0);
+          
+        }
+
         if (Input.GetKey(KeyCode.Q) && selectedRigidbody)
         {
             selectedRigidbody.transform.Rotate(new Vector3(0, -5, 0));
         }
+
+        if (Input.GetKeyDown(KeyCode.F) && selectedRigidbody)
+        {
+            selectedRigidbody.transform.Rotate(new Vector3(180, 180, 0));
+        }
+
     }
 
     void FixedUpdate()
@@ -48,13 +64,18 @@ public class ObjectPickup : MonoBehaviour
         {
             Vector3 mousePositionOffset = targetCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, selectionDistance)) - originalScreenTargetPosition;
             selectedRigidbody.velocity = (originalRigidbodyPos + mousePositionOffset - selectedRigidbody.transform.position) * forceAmount * Time.deltaTime;
+            if (selectedRigidbody.gameObject.name == "Dice" && Input.GetKey(KeyCode.R))
+            {
+                Debug.Log("Yes");
+                selectedRigidbody.AddTorque(new Vector3(1,1,1) * 1000000f* Time.deltaTime);
+            }
         }
     }
 
     Rigidbody GetRigidbodyFromMouseClick()
     {
         Ray ray = targetCamera.ScreenPointToRay(Input.mousePosition);
-        bool hit = Physics.Raycast(ray, out RaycastHit hitInfo);
+        bool hit = Physics.Raycast(ray, out RaycastHit hitInfo,rayDist,layer);
         if (hit)
         {
             if (hitInfo.collider.gameObject.GetComponent<Rigidbody>())
