3367dc9ade243b883028e7dffec9bd04e3a6f8d1
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-04-30 20:02:55 -0400

    Added ChanceSpawn script

--- Assets/Scripts/ChanceSpawn.cs
+++ Assets/Scripts/ChanceSpawn.cs
@@ -5,14 +5,31 @@ using UnityEngine;
 public class ChanceSpawn : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField] private Transform chanceSpawn;
+    [SerializeField] private GameObject[] chanceCards;
+
     void Start()
     {
-        
+        SpawnCards();
     }
 
     // Update is called once per frame
-    void Update()
+
+    private void randomize()
     {
-        
+        for (int i = 0; i < chanceCards.Length - 1; i++)
+        {
+            int rnd = Random.Range(i, chanceCards.Length);
+            Debug.Log(rnd);
+        }
+    }
+    private void SpawnCards()
+    {
+        for (int i = 0; i < chanceCards.Length; i++)
+        {
+            GameObject card = Instantiate(chanceCards[i],chanceSpawn);
+            card.transform.parent = transform;
+            card.transform.position = chanceSpawn.transform.position + new Vector3(0,0.1f*i,0);
+        }
     }
 }
